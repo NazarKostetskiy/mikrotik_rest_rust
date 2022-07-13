df217505aaa4ef7ab80f928e0b26bcae4a6c0ba6
Nazar Kostetskyi <santax@ukr.net>
2022-07-13 20:23:14 +0300

    Add pre-commit config. Add base http client impelemntation.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use client::Client;
+use reqwest::Url;
+
+mod client;
+
+// TODO: Use environment variables here
+const ROUTER_URL: &'static str = "https://192.168.88.1";
+const USERNAME: &'static str = "admin";
+const PASSWORD: &'static str = "";
 
 fn main() {
+    let mikrotik_client = Client::new(
+        Url::parse(ROUTER_URL).unwrap(),
+        USERNAME.to_string(),
+        PASSWORD.to_string(),
+        true,
+    );
 }
